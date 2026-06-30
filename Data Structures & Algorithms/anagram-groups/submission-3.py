ab579a59bed2a2432c0734076b9930929ed93fd3
perfectlava <122835703+perfectlava@users.noreply.github.com>
2026-06-30 11:47:08 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        seen = defaultdict(list)
+
+        for s in strs:
+            sign = [0] * 26
+            
+            for c in s.lower():
+                sign[ord(c) - ord('a')] += 1
+            
+            seen[tuple(sign)].append(s)
+
+        return list(seen.values())
+        
